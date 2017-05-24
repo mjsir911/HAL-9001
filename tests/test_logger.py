2d9d6985b8968eae7ae7e2462591603c100cf53b
Marco Sirabella <msirabel@gmail.com>
2017-05-24 16:39:49 -0400

    - Added first test case

---
+++ tests/test_logger.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+import tempfile
+import time
+import collections
+
+__appname__     = ""
+__author__      = "Marco Sirabella"
+__copyright__   = ""
+__credits__     = ["Marco Sirabella"]  # Authors and bug reporters
+__license__     = "GPL"
+__version__     = "1.0"
+__maintainers__ = "Marco Sirabella"
+__email__       = "msirabel@gmail.com"
+__status__      = "Prototype"  # "Prototype", "Development" or "Production"
+__module__      = ""
+
+
+class MessageCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        from HAL_9001.bot import Bot
+        from HAL_9001.abc import Address, Ident
+        addr = Address('irc.freenode.net', 6666)
+        identy = Ident('hal', 'irc.freenode.net', 'hal')
+        tmpdir = tempfile.gettempdir()
+        self.bot = HAL_9001.bot.Bot(addr, identy, tmpdir)
+        class SaySomethingThenQuit():
+            def __init__(self, bot):
+                self.bot = bot
+                self.channel = '#bot-test'
+                self.spoken = False
+            def __call__(self):
+                if not self.spoken:
+                    if self.channel not in self.bot.channels:
+                        self.bot.channels.append(channel)
+                    self.bot.action[MSG]('Hello world')
+                    self.spoken = True
+                else:
+                    self.bot.everything.remove(self)
+
+        self.bot.everything.append(SaySomethingThenQuit(self.bot))
+        super().__init__(*args, **kwargs)
+        self.bot()
+        time.sleep(1)
+
+    def testConfirmChannelConnection(self):
+        assert '#bot-test' not in self.bot.everything
+
+if __name__ == '__main__':
+    unittest.main()
